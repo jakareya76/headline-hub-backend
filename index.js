a82cf1632b32ad7390a44349570c905e6b170654
jakareya76 <jakareya1306@gmail.com>
2024-06-02 11:25:43 +0600

    connect to mongodb and load all news data

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ app.use(
   })
 );
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.73df8lc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const newsCollection = client.db("headline-hub").collection("news");
+
+    app.get("/all-news", async (req, res) => {
+      const result = await newsCollection.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
