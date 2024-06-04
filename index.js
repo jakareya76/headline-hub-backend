36a6ea09fd5263dd2787cb652f830f9a42c32edd
jakareya76 <jakareya1306@gmail.com>
2024-06-04 12:28:55 +0600

    create jwt token and verify if user is admin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -27,7 +28,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const newsCollection = client.db("headline-hub").collection("news");
+    const usersCollection = client.db("headline-hub").collection("users");
 
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res
+          .status(401)
+          .send({ message: "Unauthorized access: No authorization header" });
+      }
+
+      const token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      next();
+    };
+
+    // news api
     app.get("/all-news", async (req, res) => {
       const result = await newsCollection.find().toArray();
       res.send(result);
@@ -45,6 +81,62 @@ async function run() {
 
       const result = await newsCollection.insertOne(news);
 
+      res.send(result);
+    });
+
+    // jwt api
+    app.post("/jwt", async (req, res) => {
+      const payload = req.body;
+
+      const token = jwt.sign(payload, process.env.ACCESS_TOKEN, {
+        expiresIn: "10h",
+      });
+
+      res.send({ token });
+    });
+
+    // users api
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+
+        let admin = false;
+
+        if (user) {
+          admin = user?.role === "admin";
+        }
+
+        res.send({ admin });
+      }
+    );
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
